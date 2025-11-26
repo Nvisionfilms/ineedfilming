76e78bd90ef86cfce88e1a13c86666cc521f24c5
NVisionFilms <info@nvisionfilms.com>
2025-11-26 04:19:38 -0600

    Clean up: Remove all Supabase code, legacy SQL files, and migration docs

--- CHECK_MEETINGS_COLUMNS.sql
+++
@@ -1,6 +0,0 @@
--- Check what columns exist in the meetings table
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'meetings' 
-  AND table_schema = 'public'
-ORDER BY ordinal_position;
--- FORCE_RENAME_SCHEDULED_DATE.sql
+++
@@ -1,5 +0,0 @@
--- Force rename meetings.scheduled_date to scheduled_at
--- This will fail if the column doesn't exist, which is fine
-
-ALTER TABLE public.meetings 
-RENAME COLUMN scheduled_date TO scheduled_at;
